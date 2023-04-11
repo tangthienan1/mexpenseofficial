2c4decb153e62aeb65555ac271901b5b28a048aa
tta-multiIT <tta@multi-it.dk>
2023-04-11 09:56:04 +0700

    setup aws Amplify

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import awsConfig from './src/aws-exports';
+import { Amplify } from 'aws-amplify';
 
+Amplify.configure(awsConfig);
 AppRegistry.registerComponent(appName, () => App);
